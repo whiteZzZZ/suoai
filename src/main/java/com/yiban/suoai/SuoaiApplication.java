676584005ecedfb3c52494d299a038e5f7e2007e
505928168 <505928168@qq.com>
2019-07-09 16:16:45 +0800

    搭建

---
+++ src/main/java/com/yiban/suoai/SuoaiApplication.java
@@ -0,0 +1,13 @@
+package com.yiban.suoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuoaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuoaiApplication.class, args);
+	}
+
+}
